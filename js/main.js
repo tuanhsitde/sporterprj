046fd767926f795633b0a95bc13f9a4eb4dd4a1c
tuanhsitde <11220501@st.neu.edu.vn>
2024-03-06 00:56:00 +0700

    docs: create loginpage

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
 //learn more/see details
 // function changeDetail() {
-//     let getDetails = document.querySelector('.details .changeDetail');
-//     if (getDetails.innerHTML === "FREE AND EASY RETURN") {
-//         document.querySelector('.details .changeDetail').immerHTML = "FREE SHIPPING ON ORDER OVER $10"
-//     } else {
-//         document.querySelector('.details .changeDetail').innerHTML = "FREE AND EASY RETURN";
-//     }
-//     return getDetails.innerHTML;
+    
 // }
-// setInterval( changeDetail(), 4000);
+// setInterval( changeDetail, 4000);
+
 
 
 
