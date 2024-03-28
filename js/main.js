1712978df9348b1b18d001c3831065a7247e62c3
tuanhsitde <11220501@st.neu.edu.vn>
2024-03-28 23:50:51 +0700

    docs: new clb pages

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
-//learn more/see details
-// function changeDetail() {
-    
-// }
-// setInterval( changeDetail, 4000);
-
-
-
-
 
 //getVoucher
 let closeAds = document.querySelector('#closeX').addEventListener('click', () => {
     const addClass = document.querySelector('.btnGetVoucher').classList.add('closeAds');
 });
+
+
+
